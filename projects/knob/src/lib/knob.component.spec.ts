444ed948238a09d5531d78fffc40c8794949fc4d
Sumanth Chinthagunta <xmlking@gmail.com>
2021-05-16 19:05:28 -0700

    feat(deps): updated to angular 12
    BREAKING CHANGE: may not support angular less then 12

--- projects/knob/src/lib/knob.component.spec.ts
+++ projects/knob/src/lib/knob.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { KnobComponent } from './knob.component';
 
@@ -6,7 +6,7 @@ describe('KnobComponent', () => {
   let component: KnobComponent;
   let fixture: ComponentFixture<KnobComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ KnobComponent ]
     })
